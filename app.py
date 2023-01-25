79aa5720d49bf2f2ef0773cb37169877a5bbb202
mathieu <degueurce.mathieu@gmail.com>
2023-01-25 16:52:42 +0100

    second commit

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from Utils import database
+import sqlite3
 
 app = Flask(__name__)
+database_user = database.DataBase(user="cantina", password="LeMdPDeTest", host="localhost", port=3306,
+                                        database="cantina_db")
+database_command = sqlite3.connect('database.db')
+database_command_cursor = database_command.cursor()
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
+
+
+@app.route('/commande', methods=['POST'])
+def commande():
+    if not request.form['nd'] or not request.form["pd"] or not request.form["cd"]:
+        return redirect(url_for("home"))
+    database_command_cursor.execute()
+    return "Merci de votre commande, la fleur de {} {} sera remis le je sais plus quand".format(request.form["nd"],
+                                                                                                request.form["pd"])
 
 
 if __name__ == '__main__':
