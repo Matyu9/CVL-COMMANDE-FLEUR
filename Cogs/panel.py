5dae512de0e3184d9c2b3e8a04316be7ce45b6ec
mathieu <degueurce.mathieu@gmail.com>
2023-12-24 18:05:06 +0100

    edit léger du panel

--- Cogs/panel.py
+++ Cogs/panel.py
@@ -5,4 +5,4 @@ from flask import request, redirect, url_for, render_template
 def panel_cogs(database):
     if request.cookies.get('token') != "LOGGIN-SUCCESS":
         return redirect(url_for('login'))
-    return "tteeedt"
+    return render_template('panel/index.html')
